2648635ea4e016220761ed9135b61a96aab48bb7
kothakapusivasankarreddy <kothakapusivashankarreddy@gmail.com>
2026-01-28 09:51:05 +0530

    fs

---
+++ course database.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: 
+-- Date: 2026-01-28 09:35
+*/
+INSERT INTO `` (`coursecode`,`course name`,`faculity id`,`student id`,`faculity email`) VALUES ('CC101','CN','FAC01','VTU001','fac01@email.com');
+INSERT INTO `` (`coursecode`,`course name`,`faculity id`,`student id`,`faculity email`) VALUES ('CC102','JAVA','FAC02','VTU002','fac02@email.com');
+INSERT INTO `` (`coursecode`,`course name`,`faculity id`,`student id`,`faculity email`) VALUES ('CC103','HTML','FAC03','VTU003','fac03@email.com');
+INSERT INTO `` (`coursecode`,`course name`,`faculity id`,`student id`,`faculity email`) VALUES ('CC104','DBMS','FAC04','VTU004','fac04@email.com');
+INSERT INTO `` (`coursecode`,`course name`,`faculity id`,`student id`,`faculity email`) VALUES ('CC105','OS','FAC05','VTU005','fac05@email.com');
---
+++ student database.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: 
+-- Date: 2026-01-28 09:46
+*/
+INSERT INTO `` (`vtu number`,`name`,`email`,`phone`,`department`) VALUES ('vtu26267','sivareddy','vtu26267@veltech.edu.in','7075140509','cse');
+INSERT INTO `` (`vtu number`,`name`,`email`,`phone`,`department`) VALUES ('vtu24733','revanth','vtu24733@veltech.edu.in','7075140509','cse');
+INSERT INTO `` (`vtu number`,`name`,`email`,`phone`,`department`) VALUES ('vtu26961','krishna','vtu24961@veltech.edu.in','7075140509','cse');
+INSERT INTO `` (`vtu number`,`name`,`email`,`phone`,`department`) VALUES ('vtu24791','sidhu','vtu24791@veltech.edu.in','7075140509','cse');
+INSERT INTO `` (`vtu number`,`name`,`email`,`phone`,`department`) VALUES ('vtu24880','srinu','vtu24880@veltech.edu.in','7075140509','cse');
+INSERT INTO `` (`vtu number`,`name`,`email`,`phone`,`department`) VALUES ('24549','thipee','vtu24549@veltech.edu.in','7075140509','cse');
